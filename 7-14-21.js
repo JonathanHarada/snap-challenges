2e79bda4a98f4f0c1238357be23bbd10d278c427
Jonathan Harada <JHarada@cnm.edu>
2021-07-16 09:54:55 -0600

    object array and data code along

---
+++ 7-14-21.js
@@ -0,0 +1,29 @@
+// Function #1 Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.) Which operation is your choice.
+
+// Function declaration (a function is a main building block) //
+function multiplyTwoNumbers(number1, number2) {
+
+    console.log("first Number", number1)
+    console.log("second Number", number2)
+    console.log(result)
+    if(number1 === 20) {
+        var result = number1 * number2
+    }
+    return result
+}
+let firstNumber = 20
+let secondNumber = 5
+multiplyTwoNumbers(firstNumber, secondNumber)
+
+// Function #2 Take two strings as arguments and return a value that has them concatenated. Concatenation is a way of combinding strings //
+
+const value = "A string"
+"i am " + value + " " + "isnt Javascript fun"
+
+function combineTwoStrings(string1, string2) {
+    return string1 + string2
+}
+
+console.log(combineTwoStrings("Javascript is to carpet as", "Java is to car"))
+
+
