599f1a2fecaa25a5729de8f62f1578a72dabe994
Jonathan Harada <JHarada@cnm.edu>
2021-08-02 09:02:34 -0600

    forEach code along

---
+++ more-functions/script.js
@@ -0,0 +1,6 @@
+function handleClick() {
+    const element = document.getElementById("target")
+    element.innerHTML = "I have been clicked"
+    //remember you can set css styling using
+    // element.style
+}
